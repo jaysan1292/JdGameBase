4943e81b22ddd6a120ec5ef6efbff58863a26535
Jason Recillo <jaysan1292@gmail.com>
2013-02-23 22:45:13 -0500

    After EntityManager refactor

--- JdGameBase/Core/Services/EntityManager.cs
+++ JdGameBase/Core/Services/EntityManager.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.Linq;
 
 using JdGameBase.Camera;
+using JdGameBase.Core.GameComponents;
 using JdGameBase.Core.Interfaces;
 using JdGameBase.Extensions;
 using JdGameBase.Graphics;
@@ -16,38 +17,32 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace JdGameBase.Core.Services {
-    public class EntityManager {
-        //TODO: Remove _drawables and _updatables as IDrawableEntity and IUpdatableEntity now inherity IEntity
+    public class EntityManager : JdComponent, IDrawableEntity {
         private readonly Camera2D _camera;
         private readonly bool _containsCamera;
         private readonly Texture2D _debugTexture;
-        private readonly List<IDrawableEntity> _drawables;
-        private readonly List<Entity> _entities;
-        private readonly Game _game;
-        private readonly List<IUpdatableEntity> _updatables;
+        private readonly List<IEntity> _entities;
         public bool DrawBoundingBoxes;
 
         public Color DebugColor { get; set; }
 
-        public EntityManager(Game game) {
-            _entities = new List<Entity>();
-            _drawables = new List<IDrawableEntity>();
-            _updatables = new List<IUpdatableEntity>();
+        public EntityManager(Game game)
+            : base(game) {
+            _entities = new List<IEntity>();
             _debugTexture = ColorTexture.Create(game.GraphicsDevice, Color.White);
-            _game = game;
             DebugColor = Color.Lime;
 
             _camera = game.GetComponent<Camera2D>();
             _containsCamera = _camera != null;
         }
 
-        public int EntityCount { get { return _entities.Count + _drawables.Count + _updatables.Count; } }
+        public int EntityCount { get { return _entities.Count /*+ _drawables.Count + _updatables.Count*/; } }
 
         public int VisibleEntities {
             get {
                 return _camera == null ?
                            _entities.Count :
-                           _entities.FindAll(x => _camera.IsInView(x)).Count;
+                           _entities.OfType<Entity>().Count(x => _camera.IsInView(x));
             }
         }
 
@@ -58,30 +53,18 @@ namespace JdGameBase.Core.Services {
                 spriteBatch.DrawRectangle(_debugTexture, DebugColor, x.BoundingBox);
             };
 
-            _drawables.ForEach(x => x.Draw(spriteBatch));
-            if (_containsCamera) _entities.FindAll(x => _camera.IsInView(x) || x.AlwaysDraw).ForEach(draw);
-            else _entities.ForEach(draw);
+            _entities.OfType<IDrawableEntity>().ForEach(x => x.Draw(spriteBatch));
+            if (_containsCamera) _entities.OfType<Entity>().Where(x => _camera.IsInView(x) || x.AlwaysDraw).ForEach(draw);
+            else _entities.OfType<Entity>().ForEach(draw);
         }
 
-        public void Update(float delta) {
-            _updatables.ForEach(x => x.Update(delta));
-            _entities.ForEach(x => x.Update(delta));
+        public override void Update(float delta, GameTime gameTime) {
+            //            _updatables.ForEach(x => x.Update(delta));
+            _entities.OfType<IUpdatableEntity>().ToList().ForEach(x => x.Update(delta, gameTime));
         }
 
-        public List<T> GetAllEntities<T>() where T : Entity {
+        public List<T> GetEntitiesOfType<T>() where T : IEntity {
             return (from entity in _entities
-                    where entity.GetType().IsDerivedFrom<T>()
-                    select (T) entity).ToList();
-        }
-
-        public List<T> GetAllDrawables<T>() where T : IDrawableEntity {
-            return (from entity in _drawables
-                    where entity.GetType().IsDerivedFrom<T>()
-                    select (T) entity).ToList();
-        }
-
-        public List<T> GetAllUpdatables<T>() where T : IUpdatableEntity {
-            return (from entity in _updatables
                     where entity.GetType().IsDerivedFrom<T>()
                     select (T) entity).ToList();
         }
@@ -131,101 +114,33 @@ namespace JdGameBase.Core.Services {
 
         #region Entity Registration/Unregistration
 
-        #region IEntity
-
-        public void RegisterEntity(Entity entity) {
+        public void RegisterEntity(IEntity entity) {
             _entities.Add(entity);
         }
 
-        public void RegisterEntity(IEnumerable<Entity> ents) {
+        public void RegisterEntity(IEnumerable<IEntity> ents) {
             _entities.AddRange(ents);
         }
 
-        public void UnregisterEntity(Entity entity) {
+        public void UnregisterEntity(IEntity entity) {
             if (_entities.Contains(entity)) _entities.Remove(entity);
         }
 
-        public void UnregisterEntities(IEnumerable<Entity> ents) {
+        public void UnregisterEntities(IEnumerable<IEntity> ents) {
             _entities.RemoveAll(ents.Contains);
         }
 
-        public void UnregisterEntitiesOfType<T>() where T : Entity {
+        public void UnregisterEntitiesOfType<T>() where T : IEntity {
             _entities.RemoveAll(x => x.GetType().IsDerivedFrom<T>());
         }
 
-        public void UnregisterAllEntities<T>(Predicate<T> predicate) where T : Entity {
+        public void UnregisterAllEntities<T>(Predicate<T> predicate) where T : IEntity {
             var ents = from entity in _entities
                        where entity.GetType().IsDerivedFrom<T>()
                        where predicate.Invoke((T) entity)
                        select entity;
             UnregisterEntities(ents);
         }
-
-        #endregion
-
-        #region IDrawableEntity
-
-        public void RegisterEntity(IDrawableEntity entity) {
-            _drawables.Add(entity);
-        }
-
-        public void RegisterEntity(IEnumerable<IDrawableEntity> ents) {
-            _drawables.AddRange(ents);
-        }
-
-        public void UnregisterEntity(IDrawableEntity entity) {
-            if (_drawables.Contains(entity)) _drawables.Remove(entity);
-        }
-
-        public void UnregisterEntities(IEnumerable<IDrawableEntity> ents) {
-            _drawables.RemoveAll(ents.Contains);
-        }
-
-        public void UnregisterDrawableEntityType<T>() where T : IDrawableEntity {
-            _drawables.RemoveAll(x => x.GetType().IsDerivedFrom<T>());
-        }
-
-        public void UnregisterAllDrawableEntities<T>(Predicate<T> predicate) where T : IDrawableEntity {
-            var ents = from entity in _drawables
-                       where entity.GetType().IsDerivedFrom<T>()
-                       where predicate.Invoke((T) entity)
-                       select entity;
-            UnregisterEntities(ents);
-        }
-
-        #endregion
-
-        #region IUpdatableEntity
-
-        public void RegisterEntity(IUpdatableEntity entity) {
-            _updatables.Add(entity);
-        }
-
-        public void RegisterEntity(IEnumerable<IUpdatableEntity> ents) {
-            _updatables.AddRange(ents);
-        }
-
-        public void UnregisterEntity(IUpdatableEntity entity) {
-            if (_updatables.Contains(entity)) _updatables.Remove(entity);
-        }
-
-        public void UnregisterEntities(IEnumerable<IUpdatableEntity> ents) {
-            _updatables.RemoveAll(ents.Contains);
-        }
-
-        public void UnregisterUpdatableEntityType<T>() where T : IUpdatableEntity {
-            _updatables.RemoveAll(x => x.GetType().IsDerivedFrom<T>());
-        }
-
-        public void UnregisterAllUpdatableEntities<T>(Predicate<T> predicate) where T : IUpdatableEntity {
-            var ents = from entity in _updatables
-                       where entity.GetType().IsDerivedFrom<T>()
-                       where predicate.Invoke((T) entity)
-                       select entity;
-            UnregisterEntities(ents);
-        }
-
-        #endregion
 
         #endregion
     }
