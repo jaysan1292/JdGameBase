d095fd6251dd95f8b8d4e6035f8c2803ea7bd004
Jason Recillo <jaysan1292@gmail.com>
2013-03-07 12:56:20 -0500

    asdf

---
+++ JdGameBase/Core/Geometry/RectangularPrism.cs
@@ -0,0 +1,31 @@
+﻿// Project: JdGameBase
+// Filename: RectangularPrism.cs
+// 
+// Author: Jason Recillo
+
+using System;
+
+using Microsoft.Xna.Framework;
+
+namespace JdGameBase.Core.Geometry {
+    public struct RectangularPrism {
+        public float Length;
+        public float Width;
+        public float Height;
+        public Vector3 Position;
+        public Quaternion Rotation;
+
+        public RectangularPrism(Vector3 pos, float l, float w, float h) {
+            Position = pos;
+            Length = l;
+            Width = w;
+            Height = h;
+            Rotation = Quaternion.Identity;
+        }
+
+        public bool Intersects(RectangularPrism other) {
+            // TODO: Cube/Rectangular prism intersection
+            return false;
+        }
+    }
+}
---
+++ JdGameBase/ThreeDimensional/ThreeDimensional.cs
@@ -0,0 +1,57 @@
+﻿// Project: JdGameBase
+// Filename: ThreeDimensional.cs
+// 
+// Author: Jason Recillo
+
+using System;
+
+using JdGameBase.ThreeDimensional.Interfaces;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace JdGameBase.ThreeDimensional {
+    namespace Interfaces {
+        public interface IEntity3D : IDrawableEntity3D {
+            Vector3 Position { get; set; }
+        }
+
+        public interface IDrawableEntity3D {
+            void Draw(GraphicsDevice device, ICamera3D camera);
+        }
+
+        public interface ICamera3D {
+            float AspectRatio { get; set; }
+            Matrix World { get; }
+            Matrix View { get; }
+            Matrix Projection { get; }
+            IEntity3D Target { get; set; }
+            Vector3 Position { get; set; }
+            Vector3 UpVector { get; set; }
+        }
+    }
+
+    namespace Camera {
+        public class Camera3D : ICamera3D {
+            private Matrix _world;
+            private Matrix _view;
+            private Matrix _projection;
+
+            public Vector3 Position { get; set; }
+            public float AspectRatio { get; set; }
+            public Matrix World { get { return _world; } }
+            public Matrix View { get { return _view; } }
+            public Matrix Projection { get { return _projection; } }
+            public IEntity3D Target { get; set; }
+            public Vector3 UpVector { get; set; }
+        }
+    }
+
+    public abstract class Entity3D : IEntity3D {
+        private Vector3 _position;
+
+        public Vector3 Position { get { return _position; } set { _position = value; } }
+
+        public abstract void Draw(GraphicsDevice device, ICamera3D camera);
+    }
+}
